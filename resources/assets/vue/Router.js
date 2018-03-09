a8f228e1f788695a7ad9f41f69206719ddfee021
Mohamed Nasser <mhmeeaad@gmail.com>
2018-03-09 04:03:07 +0200

    Edit: add all profile sub routes

--- resources/assets/vue/Router.js
+++ resources/assets/vue/Router.js
@@ -3,7 +3,15 @@ import Router from 'vue-router'
 import Home from './pages/Home.vue';
 import Clothing from './pages/Clothing.vue';
 import Profile from './pages/Profile.vue';
-import Sets from './components/profile/Sets.vue';
+  //Profile sub pages
+  import ProfileSets from './components/profile/SubPageSets.vue';
+  import ProfileCollections from './components/profile/SubPageCollections';
+  import ProfileItems from './components/profile/SubPageItem.vue';
+  import ProfileLikes from './components/profile/SubPageLikes.vue';
+  import ProfileWins from './components/profile/SubPageWins.vue';
+  import ProfileFollowing from './components/profile/SubPageFollowing.vue';
+  import ProfileFollowers from './components/profile/SubPageFollowers.vue';
+  import ProfileBlockedUsers from './components/profile/SubPageBlockedUsers.vue';
 
 Vue.use(Router);
 
@@ -25,13 +33,42 @@ export default new Router({
       component: Profile,
       children: [
         {
-          path: '',
-          component: Sets
+          path: 'blockedusers',
+          component: ProfileBlockedUsers
         },
-        // {
-        //   path: 'posts',
-        //   component: UserPosts
-        // }
+        {
+          path: 'collections',
+          component: ProfileCollections
+        },
+        {
+          path: 'followers',
+          component: ProfileFollowers
+        },  
+        {
+          path: 'following',
+          component: ProfileFollowing
+        },
+        {
+          path: 'following',
+          component: ProfileFollowing
+        },
+        {
+          path: 'items',
+          component: ProfileItems
+        },
+        {
+          path: 'likes',
+          component: ProfileLikes
+        },
+        {
+          path: 'sets',
+          component: ProfileSets
+        },
+        {
+          path: 'wins',
+          component: ProfileWins
+        },
+        { path: '/', redirect: '/sets' },        
       ]
     }
   ]
