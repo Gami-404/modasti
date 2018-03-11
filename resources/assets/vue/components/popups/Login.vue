edbcf43f4e5721404e7df005e5fb8be986e7376f
Mohamed Nasser <mhmeeaad@gmail.com>
2018-03-11 20:02:55 +0200

    (Add) popups

---
+++ resources/assets/vue/components/popups/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="popupPage">
+		<div class="verticalCentered">
+			<div class="theCell">
+				
+				<div class="outerClose"></div>
+				
+				<div class="thePage">
+					<div class="head">
+						<span>Login</span>
+						<span class="icon closePopup"><i class="fa fa-close"></i></span>
+					</div>
+					<div class="content">
+						<div class="theForm">
+							<input type="text" class="formEle" placeholder="Email">
+							<input type="password" class="formEle" placeholder="Password">
+							<input type="submit" class="formEle btn" value="login">
+						</div>
+						
+						<div class="otherLinks">
+							<div class="one">Forgot Password? <a href="#">Get New</a></div>
+							<div class="one">Not Registered? <a href="#">Sign Up</a></div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</template>
