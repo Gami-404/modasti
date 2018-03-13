6eb94a1719e384e6847cf8c1b27fdf82d0226683
Mohamed Nasser <mhmeeaad@gmail.com>
2018-03-13 20:57:40 +0200

    ( Add ) routing transitions at main router

--- resources/assets/vue/App.vue
+++ resources/assets/vue/App.vue
@@ -1,21 +1,31 @@
 <template>
     <div>
         <app-navbar/>
-        <router-view/>
+        <transition name="paget"
+        enter-active-class="animated fadeIn"
+        >
+            <router-view/>
+        </transition>
         <app-footer/>
     </div>
 </template>
 
 <script>
-    import Navbar from './layout/Navbar.vue';
-    import Footer from './layout/Footer.vue';
-    export default {
-        components:{
-            'app-navbar': Navbar,
-            'app-footer': Footer
-        },
-        mounted() {
-            console.log('Vue App Mounted !')
-        }
-    }
+import Navbar from "./layout/Navbar.vue";
+import Footer from "./layout/Footer.vue";
+export default {
+  components: {
+    "app-navbar": Navbar,
+    "app-footer": Footer
+  },
+  mounted() {
+    console.log("Vue App Mounted !");
+  }
+};
 </script>
+
+<style>
+    @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css";
+
+    
+</style>
