86f2baef56855b5c6adfe52ceb592ef3a6b851df
Mohamed Nasser <mhmeeaad@gmail.com>
2018-04-19 20:07:49 +0200

    undo edits in maps.php

--- app/maps.php
+++ app/maps.php
@@ -35,7 +35,7 @@ namespace Maps\User {
         $newArray = [];
         foreach ($users as $user) {
             $newUser = new \stdClass();
-            $newUser->id = $user->userId;
+            $newUser->id = $user->id;
             $newUser->fname = $user->first_name;
             $newUser->lname = $user->last_name;
             $newUser->username = $user->username;
