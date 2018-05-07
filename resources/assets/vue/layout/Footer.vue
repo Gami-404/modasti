ccb469c70956b12e83ce3eed7e11739041103b0f
nasseratic <mhmeeaad@gmail.com>
2018-05-07 18:17:10 +0200

    ( Update ) add some static links

--- resources/assets/vue/layout/Footer.vue
+++ resources/assets/vue/layout/Footer.vue
@@ -1,48 +1,79 @@
 <template>
-  <footer id="footer">
+	<footer id="footer">
 		<div class="gridContainer clearfix">
-		
 			<div class="logoFooter">
 				<div class="theLogo"><img src="images/logo.png" alt=""></div>
 				<p>A place where women from all over the world meet the widest selection of modest clothing, a place where designers, stylists, influencers, and shoppers meet and inspire each other with new fashion and trends. Indeed the era of modest fashion has just begun.</p>
 				<div class="social">
-					<a href="#"><i class="fa fa-facebook-official"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-telegram"></i></a>
+					<a href="#">
+						<i class="fa fa-facebook-official"></i>
+					</a>
+					<a href="#">
+						<i class="fa fa-twitter"></i>
+					</a>
+					<a href="#">
+						<i class="fa fa-telegram"></i>
+					</a>
 				</div>
 			</div>
-		
 			<div class="menu">
 				<div class="footerTitle">QUICK LINKS</div>
 				<ul>
-					<li><a href="#">DATA FEED GUIDELINES AND SPECIFICATIONS</a></li>
-					<li><a href="#">Help</a></li>
-					<li><a href="#">Privacy Policy</a></li>
-					<li><a href="#">CONTACT US</a></li>
-					<li><a href="#">About Modasti</a></li>
-					<li><a href="#">Terms of service</a></li>
-					<li><router-link to="/business-with-us">Business with us</router-link></li>
+					<!-- <li>
+						<a href="#">DATA FEED GUIDELINES AND SPECIFICATIONS</a>
+					</li> -->
+					<!-- <li>
+						<a href="#">Help</a>
+					</li> -->
+					<li>
+						<a href="#">Privacy Policy</a>
+					</li>
+					<li>
+						<router-link to="/contact">CONTACT US</router-link>
+					</li>
+					<li>
+						<a href="#">About Modasti</a>
+					</li>
+					<li>
+						<a href="#">Terms of service</a>
+					</li>
+					<li>
+						<router-link to="/business-with-us">Business with us</router-link>
+					</li>
 				</ul>
 			</div>
-		
 			<div class="menu">
 				<div class="footerTitle">SUPPORT</div>
 				<ul>
-					<li><a href="#">Help</a></li>
-					<li><a href="#">Privacy Policy</a></li>
-					<li><a href="#">Terms of service</a></li>
-					<li><a href="#">CONTACT US</a></li>
-					<li><a href="#">About Modasti</a></li>
+					<li>
+						<router-link to="/help">Help</router-link>
+					</li>
+					<li>
+						<a href="#">Privacy Policy</a>
+					</li>
+					<li>
+						<router-link to="/terms">Terms of service</router-link>
+					</li>
+					<li>
+						<router-link to="/contact">CONTACT US</router-link>
+					</li>
+					<li>
+						<router-link to="/about">About Modasti</router-link>
+					</li>
 				</ul>
 			</div>
-			
 			<div class="contactFooter">
 				<div class="footerTitle">CONTACT US</div>
 				<div class="description">If you want to give us a feedback or If you have a question and you didn`t find the answer in the FAQ`s page feel free to contact us.</div>
-				<div class="info"><i class="fa fa-map-marker"></i><span>123 Second Street Fifth Avenue, Manhattan, New York</span></div>
-				<div class="info"><i class="fa fa-mail-reply"></i><a href="#">help@modasti.com</a></div>
+				<div class="info">
+					<i class="fa fa-map-marker"></i>
+					<span>123 Second Street Fifth Avenue, Manhattan, New York</span>
+				</div>
+				<div class="info">
+					<i class="fa fa-mail-reply"></i>
+					<a href="#">help@modasti.com</a>
+				</div>
 			</div>
-			
 		</div>
 	</footer>
 </template>
