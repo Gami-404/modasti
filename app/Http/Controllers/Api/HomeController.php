9943ac5ee362d1a3e3a0e937b97e7c3de58ffbbf
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-05-07 18:19:28 +0200

    Add Feed

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -204,7 +204,7 @@ class HomeController extends Controller
         $limit = $request->get('limit', 8);
         $items = Post::with('image', 'brand')->confirmed()
             ->whereHas('user.follower', function ($query) {
-                $query->where('following_id', fauth()->user()->id)
+                $query->where('following_id', fauth()->user()->id);
             })->orderBy('likes', 'desc')->offset($offset)->take($limit)->get();
         $data['data']['items'] = \Maps\Item\items($items);
         return response()->json($data);
