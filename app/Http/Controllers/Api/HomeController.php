63d6e41c549c95e1a2d2804305de9d8a492cb8de
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-06-12 17:48:25 +0200

    Add HomeController

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -210,7 +210,7 @@ class HomeController extends Controller
         $sets_best_from_community = Set::with('image')
             ->withCount('likes')
             ->where('created_at', '>=', Carbon::now()->subDay(10))
-            ->orderBy('likes', 'desc')
+            ->orderBy('likes_count', 'desc')
             ->take(4)->get();
         $data['sets_best_from_community'] = collect(\Maps\Set\sets($sets_best_from_community));
 
