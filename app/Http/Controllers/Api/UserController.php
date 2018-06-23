bdd2df60214fd4f9284b0e2f2512261ed3f9259d
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-06-23 19:33:36 +0200

    Login Test

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -56,9 +56,7 @@ class UserController extends Controller
             return response()->json($response, '400');
         }
 
-        \Log::debug(fauth()->user()->suspended_at);
-        \Log::debug(fauth()->id());
-        if (fauth()->user()->suspended_at&&fauth()->user()->suspended_at->getTimestamp() >= Carbon::now()->getTimestamp()) {
+        if (fauth()->user()->suspended_to&&fauth()->user()->suspended_to->getTimestamp() >= Carbon::now()->getTimestamp()) {
             $response['errors'] = ["Your account suspended for " . fauth()->user()->suspended_at->format('l jS \\of F Y h:i:s A')];
             return response()->json($response, '400');
         }
