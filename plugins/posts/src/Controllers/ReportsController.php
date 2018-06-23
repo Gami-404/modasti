693f406c6c771689a11a8ff03b6f80cb7f1b8988
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-06-23 17:28:44 +0200

    Action Do

--- plugins/posts/src/Controllers/ReportsController.php
+++ plugins/posts/src/Controllers/ReportsController.php
@@ -3,6 +3,9 @@
 namespace Dot\Posts\Controllers;
 
 use Action;
+use App\Model\Set;
+use Dot\Platform\Classes\Carbon;
+use Dot\Posts\Models\Collection;
 use Dot\Posts\Models\Report;
 use Illuminate\Support\Facades\Auth;
 use Dot\Platform\Controller;
@@ -86,17 +89,17 @@ class ReportsController extends Controller
 
         foreach ($ids as $ID) {
 
-            $question = Report::findOrFail($ID);
+            $report = Report::findOrFail($ID);
 
             // Fire deleting action
 
-            Action::fire("post.report.deleting", $question);
+            Action::fire("post.report.deleting", $report);
 
-            $question->delete();
+            $report->delete();
 
             // Fire deleted action
 
-            Action::fire("post.report.deleted", $question);
+            Action::fire("post.report.deleted", $report);
         }
 
         return Redirect::back()->with("message", trans("posts::reports.events.deleted"));
@@ -110,30 +113,31 @@ class ReportsController extends Controller
     public function details($id)
     {
 
-        $question = Report::findOrFail($id);
+        $report = Report::findOrFail($id);
 
-        if (Request::isMethod("post") && $question->action_id == 0) {
+        if (Request::isMethod("post") && $report->action_id == 0) {
 
 
-            $question->action_id = Request::get('action_id');
+            $report->action_id = Request::get('action_id');
             // Fire saving action
 
-            Action::fire("report.change", $question);
+            Action::fire("report.change", $report);
 
-            if (!$question->validate()) {
-                return Redirect::back()->withErrors($question->errors())->withInput(Request::all());
+            if (!$report->validate()) {
+                return Redirect::back()->withErrors($report->errors())->withInput(Request::all());
             }
 
-            $question->save();
+            $report->save();
+            $this->doActions($report, Request::get('action_id'));
 
             // Fire saved action
 
-            Action::fire("report.saved", $question);
+            Action::fire("report.saved", $report);
 
             return Redirect::route("admin.posts.reports.edit", array("id" => $id))->with("message", trans("posts::reports.events.updated"));
         }
 
-        $this->data["report"] = $question;
+        $this->data["report"] = $report;
         return View::make("posts::reports.details", $this->data);
     }
 
@@ -143,7 +147,31 @@ class ReportsController extends Controller
      */
     protected function doActions($report, $action_id)
     {
+        $object = null;
 
+        if ($report->type == "set") {
+            $object = Set::find($report->object_id);
+        }
+
+        if ($report->type == "set") {
+            $object = Collection::find($report->object_id);
+        }
+        if ($action_id == 1) {
+            $object->delete();
+        }
+
+
+        $user = $object->user;
+
+        if ($action_id == 2) {
+            $user->suspended = 1;
+        }
+
+
+        if ($action_id == 3) {
+            $user->suspended_to = Carbon::now()->addMonth();
+        }
+        $user->save();
     }
 
 }
