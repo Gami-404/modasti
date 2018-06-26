0be1681c6e55a050282dbcd7fa3d2c9f3fc76db8
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-06-26 17:46:52 +0200

    Remove form

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -443,7 +443,7 @@
                         <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td class="content-cell" align="center">
-                                    <p class="sub align-center">&copy; {{date('Y')}} Modasti. All rights reserved.</p>
+                                    <p class="sub align-center">&copy;{{date('Y')}} Modasti. All rights reserved.</p>
                                 </td>
                             </tr>
                         </table>
